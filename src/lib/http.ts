d4f220480595fe74e612b123db177be9577b7f84
coderoofsit <119448686+pashupati-singh@users.noreply.github.com>
2026-03-26 11:01:25 +0530

    ypdate

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,6 +1,6 @@
 // export const BASE_URL = "http://localhost:8000";
-export const BASE_URL = "http://217.196.50.35:8005";
-// export const BASE_URL = "https://test-exam-backend-5yh6.onrender.com";
+// export const BASE_URL = "http://217.196.50.35:8005";
+export const BASE_URL = "https://test-exam-backend-5yh6.onrender.com";
 
 export const TOKEN =
 	'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5YTAyMjAzNmY0OGI2NTM1YzA3MGFiYiIsIm93bmVkQnkiOm51bGwsIm93bmVkUm9sZSI6bnVsbCwicm9sZSI6Imluc3RpdHV0ZSIsImlhdCI6MTc3Mzc1OTY1Mn0.Zb2vZB4ErKGvEAcy8IKmgAiimKeBL455m5TnEWMtyMQ';
